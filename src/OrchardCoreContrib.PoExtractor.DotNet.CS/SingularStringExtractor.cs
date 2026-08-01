020848cf6579e82f57a80513e36fd1c12bf43cc6
Artur Dorochowicz <artur@dorochowicz.com>
2026-08-01 15:50:03 +0200

    dotnet(cs): evaluate concatenated string expressions in SingularStringExtractor

--- src/OrchardCoreContrib.PoExtractor.DotNet.CS/SingularStringExtractor.cs
+++ src/OrchardCoreContrib.PoExtractor.DotNet.CS/SingularStringExtractor.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
@@ -33,9 +33,63 @@ public class SingularStringExtractor(IMetadataProvider<SyntaxNode> metadataProvi
         {
 
             var argument = accessor.ArgumentList.Arguments.FirstOrDefault();
-            if (argument != null && argument.Expression is LiteralExpressionSyntax literal && literal.IsKind(SyntaxKind.StringLiteralExpression))
+            if (argument != null)
             {
-                result = CreateLocalizedString(literal.Token.ValueText, null, node);
+                if (TryEvaluateStringExpression(argument.Expression, out var text))
+                {
+                    result = CreateLocalizedString(text, null, node);
+                    return true;
+                }
+
+                if (argument.Expression is LiteralExpressionSyntax literal && literal.IsKind(SyntaxKind.StringLiteralExpression))
+                {
+                    result = CreateLocalizedString(literal.Token.ValueText, null, node);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static bool TryEvaluateStringExpression(ExpressionSyntax expr, out string value)
+    {
+        value = null;
+        if (expr == null) return false;
+
+        // String literal
+        if (expr is LiteralExpressionSyntax lit && lit.IsKind(SyntaxKind.StringLiteralExpression))
+        {
+            value = lit.Token.ValueText;
+            return true;
+        }
+
+        // Parenthesized expression: evaluate inner
+        if (expr is ParenthesizedExpressionSyntax par)
+        {
+            return TryEvaluateStringExpression(par.Expression, out value);
+        }
+
+        // Binary concatenation: left + right
+        if (expr is BinaryExpressionSyntax bin && bin.IsKind(SyntaxKind.AddExpression))
+        {
+            if (TryEvaluateStringExpression(bin.Left, out var left) && TryEvaluateStringExpression(bin.Right, out var right))
+            {
+                value = left + right;
+                return true;
+            }
+
+            return false;
+        }
+
+        // Interpolated string with no interpolations
+        if (expr is InterpolatedStringExpressionSyntax interpolated)
+        {
+            // Accept only when there are no interpolation expressions
+            if (!interpolated.Contents.OfType<InterpolationSyntax>().Any())
+            {
+                var parts = interpolated.Contents.OfType<InterpolatedStringTextSyntax>().Select(p => p.TextToken.ValueText);
+                value = string.Concat(parts);
                 return true;
             }
         }
